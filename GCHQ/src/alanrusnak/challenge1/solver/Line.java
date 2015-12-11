46294a44dc67e1bf1f3ba154f0a899d1647b4e2c
alanrusnak9 <alanrusnak9@gmail.com>
2015-12-11 22:45:53 +0000

    Made Square into enum instead of class for simplicity. Trying to implement calculatePossibilities in Line. Added tests for calculatePossibilities.

--- GCHQ/src/alanrusnak/challenge1/solver/Line.java
+++ GCHQ/src/alanrusnak/challenge1/solver/Line.java
@@ -5,6 +5,8 @@ public class Line implements Comparable<Line>{
 	private final int id;
 	private int[] blocks;
 	private Square[] squares;
+	private int possibilities;
+	
 	public Line(int id, int[] blocks, Square[] squares) {
 		super();
 		this.id = id;
@@ -12,14 +14,67 @@ public class Line implements Comparable<Line>{
 		this.squares = squares;
 	}
 
-	private int possibilities;
-	
 	public int calculatePossibilities(){
+		return calculatePossibilities(0,0);
+	}
+	
+	public int calculatePossibilities(int blocksUsed, int squaresUsed){
+		if(blocks.length-blocksUsed==0) return calculateForZero(blocksUsed,squaresUsed);
+		if(blocks.length-blocksUsed==1) return calculateForOne(blocksUsed,squaresUsed);
+		
+		int ifBlockUsed = 0;
+		int ifBlockNotUsed = 0;
+		
+		if(blockCanBeUsedHere(blocks[blocksUsed], squares, squaresUsed)){
+			ifBlockUsed = calculatePossibilities(blocksUsed+1, squaresUsed+blocks[blocksUsed]+1);			
+		}
+		ifBlockNotUsed = calculatePossibilities(blocksUsed,squaresUsed+1);
+		
+		return ifBlockUsed + ifBlockNotUsed;
 		
 	}
 	
+	
+
+	private int calculateForOne(int blocksUsed, int squaresUsed) {
+		if(blockCanBeUsedHere(blocks[blocksUsed], squares, squaresUsed)) return 1;
+		return 0;
+	}
+
+	private int calculateForZero(int blocksUsed, int squaresUsed) {
+		if(squares.length>=squaresUsed)	return 1;
+		return 0;
+	}
+
+	
+	private boolean isNoSquaresLeft(Square[] squares,int squaresUsed){
+		return squares.length-squaresUsed==0;
+	}
+	
+	private boolean isNoBlocksLeft(int[] blocks, int blocksUsed){
+		return blocks.length-blocksUsed==0;
+	}
+	
+	private boolean isLastBlock(int[] blocks, int blocksUsed){
+		return blocks.length-blocksUsed==1;
+	}
+	
+	private int possibleSquaresLeft(Square[] squares, int squaresUsed){
+		return squares.length-squaresUsed;
+	}
+	
+	private boolean blockCanBeUsedHere(int blockLength, Square[] squares, int squaresUsed) {
+		if(possibleSquaresLeft(squares,squaresUsed)<blockLength) return false;
+		if(squares[squaresUsed+blockLength]==Square.BLACK) return false;
+		for(int i = squaresUsed;i<squaresUsed+blockLength-1;i++){
+			if(squares[i]==Square.WHITE) return false;
+		}
+		return true;
+	}
+
+	
 	public int getPossibilities(){
-		
+		return possibilities;
 	}
 	
 	@Override
--- GCHQ/src/alanrusnak/challenge1/solver/Square.java
+++ GCHQ/src/alanrusnak/challenge1/solver/Square.java
@@ -1,25 +1,5 @@
 package alanrusnak.challenge1.solver;
 
-public class Square {
-	private final int id;
-	private State state;
-	
-		
-	public Square(int id, State state) {
-		super();
-		this.id = id;
-		this.state = state;
-	}
-	
-	public State getState() {
-		return state;
-	}
-	public void setState(State state) {
-		this.state = state;
-	}
-	public int getId() {
-		return id;
-	}
-		
-	
+public enum Square {
+	UNDECIDED,WHITE,BLACK
 }
