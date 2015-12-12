8825af575737845d9037eb9fc6a6abd37753b6fd
alanrusnak9 <alanrusnak9@gmail.com>
2015-12-12 11:03:38 +0000

    calculatePossibilities() passes more tests

--- GCHQ/src/alanrusnak/challenge1/tests/SolverTests.java
+++ GCHQ/src/alanrusnak/challenge1/tests/SolverTests.java
@@ -43,7 +43,11 @@ public class SolverTests {
 	
 	@Test
 	public void testCalculatePossibilitiesWithWhite() {
-	
+		assertEquals(0, (new Line(0,new int[]{1}, new Square[]{Square.WHITE})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1}, new Square[]{Square.WHITE,Square.WHITE})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1}, new Square[]{Square.WHITE,Square.UNDECIDED,Square.WHITE})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{2}, new Square[]{Square.UNDECIDED,Square.WHITE,Square.UNDECIDED})).calculatePossibilities());
+		assertEquals(2, (new Line(0,new int[]{2}, new Square[]{Square.UNDECIDED,Square.UNDECIDED,Square.WHITE,Square.UNDECIDED,Square.UNDECIDED})).calculatePossibilities());
 	}
 
 	
