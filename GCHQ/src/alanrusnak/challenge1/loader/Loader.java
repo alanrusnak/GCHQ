4b8639523aeba925c828c94cced40f24ad2a5347
alanrusnak9 <alanrusnak9@gmail.com>
2015-12-14 23:08:08 +0000

    Loader has one function which loads the whole Board.

--- GCHQ/src/alanrusnak/challenge1/loader/Loader.java
+++ GCHQ/src/alanrusnak/challenge1/loader/Loader.java
@@ -1,27 +1,11 @@
 package alanrusnak.challenge1.loader;
 
-import alanrusnak.challenge1.solver.Line;
-import alanrusnak.challenge1.solver.Square;
+import alanrusnak.challenge1.solver.Board;
 
 public class Loader {
 
-	public int loadWidth(String filepath){
 	
-	}
-	
-	public int loadHeight(String filename){
-		
-	}
-	
-	public Line[] loadHorizontalLines(String filename){
-		
-	}
-	
-	public Line[] loadVerticalLines(String filename){
-		
-	}
-	
-	public Square[][] loadBoard(String filename){
+	public Board loadBoard(String filename){
 		
 	}
 	
