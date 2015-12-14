ea529467d1f6b89ff0737cd04f2b49119386fd41
alanrusnak9 <alanrusnak9@gmail.com>
2015-12-14 23:07:47 +0000

    Board only stores lines and not squares.

--- GCHQ/src/alanrusnak/challenge1/solver/Board.java
+++ GCHQ/src/alanrusnak/challenge1/solver/Board.java
@@ -1,13 +1,61 @@
 package alanrusnak.challenge1.solver;
 
-import java.util.TreeSet;
 
 public class Board {
 
 	private int width,height;
 	private Line[] horizontalLines;
 	private Line[] verticalLines;
-	private Square[][] grid;
+	
+	
+	public Board(){
+		super();
+	}
+	
+	public Board(int width, int height, Line[] horizontalLines,	Line[] verticalLines) {
+		super();
+		this.width = width;
+		this.height = height;
+		this.horizontalLines = horizontalLines;
+		this.verticalLines = verticalLines;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public Line[] getHorizontalLines() {
+		return horizontalLines;
+	}
+
+	public void setHorizontalLines(Line[] horizontalLines) {
+		this.horizontalLines = horizontalLines;
+	}
+
+	public Line[] getVerticalLines() {
+		return verticalLines;
+	}
+
+	public void setVerticalLines(Line[] verticalLines) {
+		this.verticalLines = verticalLines;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
