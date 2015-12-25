0c9f4330404eb9e50d7a0462b8c4fa0aa75f678c
alanrusnak9 <alanrusnak9@gmail.com>
2015-12-25 20:40:40 +0000

    Resolved errors in SolverTest

--- GCHQ/src/alanrusnak/challenge1/tests/SolverTests.java
+++ GCHQ/src/alanrusnak/challenge1/tests/SolverTests.java
@@ -5,64 +5,65 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import alanrusnak.challenge1.solver.Line;
+import alanrusnak.challenge1.solver.Square;
 import alanrusnak.challenge1.solver.SquareState;
 
 public class SolverTests {
 
 	@Test
 	public void testCalculatePossibilitiesForOne() {
-		assertEquals(0, (new Line(0,new int[]{1}, new SquareState[]{})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{1}, new SquareState[]{SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(2, (new Line(0,new int[]{1}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{2}, new SquareState[]{})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(2, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(3, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1}, new Square[]{})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(2, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{2}, new Square[]{})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(2, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(3, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
 		
 		}
 	
 	@Test
 	public void testCalculatePossibilitiesForTwo() {
-		assertEquals(0, (new Line(0,new int[]{1,2}, new SquareState[]{})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{1,2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{1,2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(3, (new Line(0,new int[]{1,2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1,2}, new Square[]{})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1,2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1,2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(3, (new Line(0,new int[]{1,2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
 	
 	}
 	
 	@Test
 	public void testCalculatePossibilitiesWithBlack() {
-		assertEquals(1, (new Line(0,new int[]{1}, new SquareState[]{SquareState.BLACK,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{1}, new SquareState[]{SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(2, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{1,1}, new SquareState[]{SquareState.BLACK,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{1,1}, new SquareState[]{SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(2, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1,1}, new Square[]{new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1,1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
 	
 	}
 	
 	@Test
 	public void testCalculatePossibilitiesWithWhite() {
-		assertEquals(0, (new Line(0,new int[]{1}, new SquareState[]{SquareState.WHITE})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{1}, new SquareState[]{SquareState.WHITE,SquareState.WHITE})).calculatePossibilities());
-		assertEquals(1, (new Line(0,new int[]{1}, new SquareState[]{SquareState.WHITE,SquareState.UNDECIDED,SquareState.WHITE})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.WHITE,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(2, (new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.WHITE,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.WHITE)})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.WHITE),new Square(SquareState.WHITE)})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1}, new Square[]{new Square(SquareState.WHITE),new Square(SquareState.UNDECIDED),new Square(SquareState.WHITE)})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.WHITE),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(2, (new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.WHITE),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
 	}
 	
 	@Test
 	public void testCalculatePossibilitiesComplex() {
-		assertEquals(1, (new Line(0,new int[]{1,10,1}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED})).calculatePossibilities());
-		assertEquals(0, (new Line(0,new int[]{1,10,1}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.WHITE})).calculatePossibilities());
+		assertEquals(1, (new Line(0,new int[]{1,10,1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)})).calculatePossibilities());
+		assertEquals(0, (new Line(0,new int[]{1,10,1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.WHITE)})).calculatePossibilities());
 		}
 
 	@Test
 	public void testCompareLines() {
-		Line line0 = new Line(0,new int[]{1}, new SquareState[]{SquareState.UNDECIDED,SquareState.BLACK,SquareState.UNDECIDED});
-		Line line1 = new Line(0,new int[]{1}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED});
-		Line line2 = new Line(0,new int[]{2}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED});
-		Line line3 = new Line(0,new int[]{3}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED});
-		Line line4 = new Line(1,new int[]{3}, new SquareState[]{SquareState.UNDECIDED,SquareState.UNDECIDED,SquareState.UNDECIDED});
+		Line line0 = new Line(0,new int[]{1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.BLACK),new Square(SquareState.UNDECIDED)});
+		Line line1 = new Line(0,new int[]{1}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)});
+		Line line2 = new Line(0,new int[]{2}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)});
+		Line line3 = new Line(0,new int[]{3}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)});
+		Line line4 = new Line(1,new int[]{3}, new Square[]{new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED),new Square(SquareState.UNDECIDED)});
 		
 		assertEquals(-1,line0.compareTo(line1));
 		assertEquals(1,line1.compareTo(line2));
